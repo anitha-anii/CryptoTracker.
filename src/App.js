aee5aad28b0df8a2b4a9f03e7a8113e75356656f
anitha-anii <anithachowdary355@gmail.com>
2023-09-21 18:10:58 +0530

    first'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './Componets/Header';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './Componets/Dashboard/Dashboard';
+import Compare from './Componets/Compare/Compare';
+import Home from './Componets/Home/Home';
+import Pagedetails from './Componets/Dashboard/pagedetails';
+import 'chart.js/auto';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/compare' element={<Compare />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/coin/:id' element={<Pagedetails />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+ 
